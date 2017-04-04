8d50aaf3b0d1aa1ee5018e802a2062276aac69bf
horaciodev <horaciodev@gmail.com>
2017-04-04 06:50:01 -0700

    Adding CLI-compliant missing files

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 // main entry point
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
 import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
--- karma.conf.js
+++ karma.conf.js
@@ -10,93 +10,47 @@ module.exports = function(config) {
 
   config.set({
     basePath: '',
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'angular-cli'],
 
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter')
+      require('karma-remap-istanbul'),
+      require('angular-cli/plugins/karma')
     ],
-
-    client: {
-      builtPaths: [appBase, testingBase], // add more spec base paths as needed
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    files: [
+      { pattern: './app/test.ts', watched: false }
+    ],
+    preprocessors: {
+      './app/test.ts': ['angular-cli']
     },
-
-    proxies :{
-      "/app/": appAssets
+    mime: {
+      'text/x-typescript': ['ts','tsx']
     },
-
-    customLaunchers: {
-      // From the CLI. Not used here but interesting
-      // chrome setup for travis CI using chromium
-      Chrome_travis_ci: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
+    remapIstanbulReporter: {
+      reports: {
+        html: 'coverage',
+        lcovonly: './coverage/coverage.lcov'
       }
     },
-
-    files: [
-      // System.js for module loading
-      'node_modules/systemjs/dist/system.src.js',
-
-      // Polyfills
-      'node_modules/core-js/client/shim.js',
-
-      // zone.js
-      'node_modules/zone.js/dist/zone.js',
-      'node_modules/zone.js/dist/long-stack-trace-zone.js',
-      'node_modules/zone.js/dist/proxy.js',
-      'node_modules/zone.js/dist/sync-test.js',
-      'node_modules/zone.js/dist/jasmine-patch.js',
-      'node_modules/zone.js/dist/async-test.js',
-      'node_modules/zone.js/dist/fake-async-test.js',
-
-      // RxJs
-      { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
-      { pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false },
-
-      // Paths loaded via module imports:
-      // Angular itself
-      { pattern: 'node_modules/@angular/**/*.js', included: false, watched: false },
-      { pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: false },
-
-      { pattern: 'systemjs.config.js', included: false, watched: false },
-      { pattern: 'systemjs.config.extras.js', included: false, watched: false },
-      'karma-test-shim.js', // optionally extend SystemJS mapping e.g., with barrels
-
-      // transpiled application & spec code paths loaded via module imports
-      { pattern: appBase + '**/*.js', included: false, watched: true },
-      { pattern: testingBase + '**/*.js', included: false, watched: true },
-
-
-      // Asset (HTML & CSS) paths loaded via Angular's component compiler
-      // (these paths need to be rewritten, see proxies section)
-      { pattern: appBase + '**/*.html', included: false, watched: true },
-      { pattern: appBase + '**/*.css', included: false, watched: true },
-
-      // Paths for debugging with source maps in dev tools
-      { pattern: appSrcBase + '**/*.ts', included: false, watched: false },
-      { pattern: appBase + '**/*.js.map', included: false, watched: false },
-      { pattern: testingSrcBase + '**/*.ts', included: false, watched: false },
-      { pattern: testingBase + '**/*.js.map', included: false, watched: false}
-    ],
-
+    angularCli: {
+      config: './angular-cli.json',
+      environment: 'dev'
+    },
     // Proxied base paths for loading assets
     proxies: {
       // required for component assets fetched by Angular's compiler
       "/app/": appAssets
     },
-
     exclude: [],
-    preprocessors: {},
-    reporters: ['progress', 'kjhtml'],
-
+    reporters: config.angularCli && config.angularCli.codeCoverage
+              ? ['progress', 'karma-remap-istanbul']
+              : ['progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false
-  })
-}
+  });
+};
